a5faa964d5a84d509a5231bc63c98a35a9fe7b2c
Felicia Hoeft <fhoeft92615@gmail.com>
2023-02-27 09:45:55 -0600

    Created initial components

---
+++ src/components/Post/Post.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Post.scss';
+
+const Post = () => (
+  <div className="Post">
+    Post Component
+  </div>
+);
+
+Post.propTypes = {};
+
+Post.defaultProps = {};
+
+export default Post;
---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import "./Posts.scss"
+
+class Posts extends Component {
+    render() {
+        return (
+            <div className="Posts">
+                Our posts go here
+            </div>
+        );
+    }
+}
+
+Posts.propTypes = {};
+
+export default Posts;
